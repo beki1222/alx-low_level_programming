43b51141346c0c8c19601897fbe55ccaf185d08b
Bereket <bfikadu96@gmail.com>
2022-06-02 15:05:42 -0700

    task4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-char *s = "\"Programming is like building a multilingual puzzle"\n";
+char *s = "\"Programming is like building a multilingual puzzle";
 puts(s);
 return (0);
 }
