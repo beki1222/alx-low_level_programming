3d685d0fad581211e77d1bad3059abdb70fa37e1
Bereket <bfikadu96@gmail.com>
2022-06-03 03:50:27 -0700

    task6. Size is not grandeur, and territory does not make a natio

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int:%d byte(s)\n", sizeof(int));
+printf("Size of a char: %dbyte(s)\n", sizeof(char));
+printf("size of an int:%dbyte(s)\n", sizeof(int));
 printf("size of a long int:%d byte(s)\n", sizeof(long int));
 printf("size of a long long int:%d byte(s)\n", sizeof(long long int));
 prtintf("size of a float:%d byte(s)\n", sizeof(float));
