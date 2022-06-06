204682b3ed715352987229b122f646875b52ecc8
Bereket <bfikadu96@gmail.com>
2022-06-06 14:40:11 -0700

    task12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ *
+ *  * main - prints all possible different combinations of three digits.
+ *
+ *   * followed by a new line
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+			        putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
