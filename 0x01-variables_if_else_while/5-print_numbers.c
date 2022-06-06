c1c1debe5916ff0169f86ebf3af53ef96851b784
Bereket <bfikadu96@gmail.com>
2022-06-06 12:42:42 -0700

    task5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,7 +6,8 @@
  * main - Prints base 10 numbers
  * @void: Empty parameter list for main.
  *
- * Description: Prints numbers 0-9 
+ *
+ *Description: Prints numbers 0-9 
  *
  * Return: 0 for success
  */
