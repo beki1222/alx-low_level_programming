92646b41ae41b449acbadec368c52cd7bbed30c4
Bereket <bfikadu96@gmail.com>
2022-06-06 12:30:00 -0700

    task5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,6 +3,7 @@
 /**
  * main - Prints alphabet*
  * @void: Empty parameter list for main.
+ *
  * Description: Prints all single digit numbers of base 10 starting from 0 
  *
  * Return: 0 for success
