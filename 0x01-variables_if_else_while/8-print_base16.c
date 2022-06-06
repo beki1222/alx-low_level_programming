a81a34448cdfc1b2d1cc59024ecd7962a1cb53b6
Bereket <bfikadu96@gmail.com>
2022-06-06 13:49:34 -0700

    task8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
+ * main -  prints all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
