a2e3116901fa821b0cdf1880ed4874fea3f39439
Bereket <bfikadu96@gmail.com>
2022-06-06 13:59:49 -0700

    task9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 58;
+
+	while (num < 68)
+	{
+		putchar(num);
+		if (num < 67)
+		{
+			putchar(',');
+			putcahr(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
