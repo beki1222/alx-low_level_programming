aeb4ab310e887a699261c4241e87f13badaa9c15
Bereket <bfikadu96@gmail.com>
2022-06-06 12:16:04 -0700

    task5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet*
+ * @void: Empty parameter list for main.
+ *     
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ *  
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char num = 0;
+
+	while ( num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
