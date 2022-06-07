dd4b008272130f3da0f20eca45f18ba142cd05d2
Bereket <bfikadu96@gmail.com>
2022-06-07 12:33:44 -0700

    task5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		print('+');
+		_putchar('+');
 		return(1);
 	}
 	else if (n == 0)
 	{
-		print('0');
+		_putchar('0');
 		return(0);
 	}
 	else
 	{
-		print('-');
+		_putchar('-');
 		return(-1);
 	}
 }
