1f9c006bbdc5785e4c8b42445b161f5b03747724
Bereket <bfikadu96@gmail.com>
2022-06-07 14:05:14 -0700

    task11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
+ * print_to_98 - print all natural numbers from input to 98
+ * @n: starting point
  *
- *
- *
+ * Return: void returns nothing
  */
 
 void print_to_98(int n)
