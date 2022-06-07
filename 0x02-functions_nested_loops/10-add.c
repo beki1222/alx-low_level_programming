fcc42dab4545790d3d3c45987e3cab65796f7cce
Bereket <bfikadu96@gmail.com>
2022-06-07 13:50:04 -0700

    task10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two ints
+ * @i: first int to add
+ * @j: second int to add
+ * Return: sum of both ints
+ */
+
+int add(int i, int j, sum)
+{
+	sum = i + j;
+	
+	_putchar(sum);
+}
