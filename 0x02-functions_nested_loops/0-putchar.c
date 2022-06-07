b8ee73db62bd9d735398487a4bc36fc5bfed3c2c
Bereket <bfikadu96@gmail.com>
2022-06-07 07:41:55 -0700

    task0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<header.h>
+
+/**
+ * main - prints _putchar,
+ * followd by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char text = _putchar;
+
+	putchar(text);
+	putchar('\n');
+	return (0);
+}
