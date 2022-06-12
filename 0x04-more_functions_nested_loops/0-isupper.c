cfdbc68bfbc058d51478950eed5c6bcda11aebe1
Bereket <bfikadu96@gmail.com>
2022-06-12 10:31:40 -0700

    task0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,15 +8,15 @@
 
 int _isupper(int c)
 {
-	char uppercase = 'A';
-        int isupper = 0;
-	for (; uppercase <= 'Z'; uppercase++)
-        {
-		if (c == uppercase)
-                {
-			isupper = 1;
-                        break;
-		}
-	}
-	return (isupper);
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 1;
+break;
+}
+}
+return (isupper);
 }
